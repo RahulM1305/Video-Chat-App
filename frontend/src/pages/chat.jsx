401c26f9faa3cb3125b0711c89413ffccaebeb3a
Rahul Mishra <rahulamishra1305@gmail.com>
2024-02-29 20:14:22 +0530

    VCA

--- frontend/src/pages/chat.jsx
+++ frontend/src/pages/chat.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useCallback, useState } from "react";
 import { useParams } from "react-router-dom";
-import ReactPlayer from "react-player";
+import { ReactPlayer } from "react-player";
 import peer from "../services/peer";
 import { useSocket } from "../context/socketProvider";
 import "../css/chat.css";
@@ -64,7 +64,7 @@ function Chat() {
   }, [myStream]);
 
   const handleCallAccepted = useCallback(
-    ({ ans }) => {
+    ({ from, ans }) => {
       peer.setLocalDescription(ans);
       console.log("Call Accepted!");
       sendStreams();
